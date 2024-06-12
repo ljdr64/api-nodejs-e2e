61093d362888ace9e9b24d89ae8bfdd6b10d5eac
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-06-12 08:02:29 -0300

    Pruebas a rutas con proteccion

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const createApp = require('./../src/app');
+const { config } = require('../src/config/config');
 
 describe('tests for app', () => {
   let app = null;
@@ -18,6 +19,27 @@ describe('tests for app', () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('laure');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  describe('GET /nueva-ruta', () => {
+    test('should return 401', async () => {
+      const { statusCode } = await api.get('/nueva-ruta');
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return 401 with invalid apiKey', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: '1212',
+      });
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return 200', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: config.apiKey,
+      });
+      expect(statusCode).toEqual(200);
+    });
   });
 
   afterEach(() => {
